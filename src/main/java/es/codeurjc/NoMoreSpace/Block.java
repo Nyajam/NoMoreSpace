85586d93e15491d8b2364dd6f7d1604818116b6b
j.lamparero <j.lamparero@DESKTOP-T8GEV19>
2021-02-18 12:41:37 +0100

    .

--- src/main/java/es/codeurjc/NoMoreSpace/Block.java
+++ src/main/java/es/codeurjc/NoMoreSpace/Block.java
@@ -1,7 +1,14 @@
 package es.codeurjc.NoMoreSpace;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Block {
 
+	@Id
+	@GeneratedValue
 	private long block_id;
 	
 }
